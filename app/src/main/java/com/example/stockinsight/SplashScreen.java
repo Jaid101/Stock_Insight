55857ac2ddbd4a15d048a0c6ad9ca36003555923
Jaid101 <jaidtamboli101@gmail.com>
2024-10-12 21:13:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stockinsight/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.stockinsight;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home_screen = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home_screen);
+
+                finish();
+            }
+        }, 3000);
+
+    }
+}
